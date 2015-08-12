8186c89d03c621d018de0a8a243ab1563c0d183c
ricardison <ricardison@GRDTI04>
2015-08-12 18:00:09 -0300

    imprimindo na tela

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,8 +15,9 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int a=6;
+        System.out.println("Primeiro arquivo"+a);
         
-        System.out.println("Primeiro arquivo");
     }
     
 }
